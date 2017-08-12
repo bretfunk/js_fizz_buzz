9455c3947247d3df2bc95becde2bf7df570dc0aa
BFunk <bret.e.funk@gmail.com>
2017-08-12 17:00:34 -0600

    fizzbuzz in JS

---
+++ app.js
@@ -0,0 +1,17 @@
+var number = 50
+
+function fizzBuzz(num) {
+  var output = ""
+  if (num % 3 == 0) {
+    output += "fizz"
+  }
+  if (num % 5 == 0) {
+    output += "buzz"
+  }
+  console.log(output || i)
+}
+
+for (i = 0; i <= 50; i++) {
+  fizzBuzz(i)
+}
+
